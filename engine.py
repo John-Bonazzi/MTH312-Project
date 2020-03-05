4447f47e04765610efa01ab874f909132278d034
John Bonazzi <gionatabonazzi@gmail.com>
2020-03-05 15:47:07 -0500

    Changed strings to work as lists, added the "give hint" function

--- engine.py
+++ engine.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     passw = Password("hello")
     print(passw.get_password())
     print(passw.get_password_plain())
+    print(passw.get_pos())
+    passw.give_hint()
+    print(passw.get_password())
+    print(passw.get_pos())
--- password.py
+++ password.py
@@ -1,20 +1,35 @@
+import secrets
+
 class Password:
-    password_plain = ''
+    password_plain = []
 
     password = ''
 
-    def __init__(self, password):
+    unknown_positions = []
+
+    def __init__(self, password, length_limit=16, fixed_length=False):
+        if length_limit:
+            pass
         self.set_password(password)
+        self.unknown_positions = list(range(0, len(self.password_plain)))
 
     def set_password(self, new_password):
-        self.password_plain = new_password
-        self.hide_password(self.password_plain)
+        self.password_plain = list(new_password)
+        self.hide_password(new_password)
 
     def hide_password(self, string):
-        self.password = '*'*len(string)
+        self.password = list('*'*len(string))
 
     def get_password(self):
-        return self.password
+        return "".join(self.password)
 
     def get_password_plain(self):
-        return self.password_plain
+        return "".join(self.password_plain)
+
+    def give_hint(self):
+        position = secrets.choice(self.unknown_positions)
+        self.password[position] = self.password_plain[position]
+        self.unknown_positions.remove(position)
+
+    def get_pos(self):
+        return self.unknown_positions
