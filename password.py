4ef12907acf1b671aceab492cf7e54b8c796bb44
John Bonazzi <gionatabonazzi@gmail.com>
2020-03-05 18:00:39 -0500

    timer works repeatedly, no crashes

--- password.py
+++ password.py
@@ -1,5 +1,7 @@
 import secrets
 from threading import Timer
+import sched
+import time
 
 class Password:
     password_plain = []
@@ -10,14 +12,19 @@ class Password:
 
     gui_update = None
 
+    timer = None
+
     def __init__(self, password, gui_update=None, length_limit=16, fixed_length=False):
         if length_limit:
             pass
         self.set_password(password)
         self.gui_update = gui_update
         self.unknown_positions = list(range(0, len(self.password_plain)))
-        t = Timer(3.0, self.give_hint)
-        t.start()
+        """self.timer = sched.scheduler(time.time, time.sleep)
+        self.timer.enter(5, 1, self.give_hint)
+        self.timer.run()"""
+        self.timer = Timer(1.0, self.give_hint)
+        self.timer.start()
         
 
     def set_password(self, new_password):
@@ -34,11 +41,20 @@ class Password:
         return "".join(self.password_plain)
 
     def give_hint(self):
+        # self.timer.enter(5, 1, self.give_hint)
+        if len(self.unknown_positions) == 0:
+            return None
+        self.timer = Timer(1.0, self.give_hint)
+        self.timer.start()
         position = secrets.choice(self.unknown_positions)
         self.password[position] = self.password_plain[position]
         self.unknown_positions.remove(position)
         if self.gui_update is not None:
             self.gui_update(self.get_password())
+        #self.timer.start()
+
+    def game_over():
+        self.gui_update(self.get_password())
 
     def get_pos(self):
         return self.unknown_positions
