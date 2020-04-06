62ac9be3bfd0b1f6849ee669b4c57de0509785d0
Ben Brown <brownb4@mail.gvsu.edu>
2020-04-06 17:07:24 -0400

    fixed GUI crash/ improved GUI overall

--- testGUI.py
+++ testGUI.py
@@ -4,8 +4,6 @@ from password import Password
 from tkinter import messagebox
 import re
 
-FLAG = True
-
 
 window = Tk()
 
@@ -14,7 +12,7 @@ window.title("MTH312 Project")
 window.geometry('350x250')
 
 diff = IntVar()
-window.passDispFlag = True
+# window.passDispFlag = True
 window.passLimit = 16
 
 
@@ -27,24 +25,24 @@ def testVal(P):
 
 
 def passwordUpdate(message):
-    if message == "GAME OVER":
-        enterBtn.configure(state="enabled")
-        passBox.configure(state="enabled")
-        window.passDispFlag = False
-        messagebox.showinfo("YOU WIN!", "The test took too long and timed out,\n "
-                                        "congratulations! Your Password is secure")
-        knownLbl.configure(text="")
-    elif window.passDispFlag:
-        knownLbl.configure(text=message)
+    # if message == "GAME OVER":
+    #    enterBtn.configure(state="enabled")
+    #    passBox.configure(state="enabled")
+    #    window.passDispFlag = False
+    #    messagebox.showinfo("YOU WIN!", "The test took too long and timed out,\n "
+    #                                    "congratulations! Your Password is secure")
+    #    knownLbl.configure(text="")
+    # elif window.passDispFlag:
+    knownLbl.configure(text=message)
     window.update()
 
 
 def checkStrength():
     if len(passBox.get()) > 0:
         Password(passBox.get(), passwordUpdate)
-        enterBtn.configure(state="disabled")
-        passBox.configure(state="disabled")
-        window.passDispFlag = True
+        # enterBtn.configure(state="disabled")
+        # passBox.configure(state="disabled")
+        # window.passDispFlag = True
     else:
         knownLbl.configure(text="enter a password to test")
 
@@ -53,9 +51,6 @@ def about():
     messagebox.showinfo('MTH312 Project', "A MTH312 project by:\nJohn Bonazzi and Benjamin Brown")
 
 
-#easyRad = Radiobutton(window, text='Easy', value=1, variable=diff)
-#medRad = Radiobutton(window, text='Medium', value=2, variable=diff)
-#hardRad = Radiobutton(window, text='Hard', value=3, variable=diff)
 passLbl = Label(window, text="Enter \"Password\":")
 passBox = Entry(window, width=20, validate="key")
 passBox.configure(validatecommand=(passBox.register(testVal), '%P'))
@@ -74,9 +69,6 @@ passBox.grid(column=0, row=1)
 enterBtn.grid(column=1, row=1)
 knownLbl.grid(column=0, row=2)
 timeLbl.grid(column=0, row=3)
-#easyRad.grid(column=0, row=4)
-#medRad.grid(column=1, row=4)
-#hardRad.grid(column=2, row=4)
 
 window.config(menu=fileMenu)
 passBox.focus()
