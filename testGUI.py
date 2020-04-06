dabe43fc5ddf17d78c0d2259b238b9e6514bf16f
Ben Brown <brownb4@mail.gvsu.edu>
2020-04-06 17:21:59 -0400

    fixed GUI duplication

--- testGUI.py
+++ testGUI.py
@@ -4,16 +4,16 @@ from password import Password
 from tkinter import messagebox
 import re
 
+if __name__ == "__main__":
+    window = Tk()
 
-window = Tk()
 
+    window.title("MTH312 Project")
+    window.geometry('350x250')
 
-window.title("MTH312 Project")
-window.geometry('350x250')
-
-diff = IntVar()
-# window.passDispFlag = True
-window.passLimit = 16
+    diff = IntVar()
+    # window.passDispFlag = True
+    window.passLimit = 16
 
 
 def testVal(P):
@@ -50,29 +50,29 @@ def checkStrength():
 def about():
     messagebox.showinfo('MTH312 Project', "A MTH312 project by:\nJohn Bonazzi and Benjamin Brown")
 
+if __name__ == "__main__":
+    passLbl = Label(window, text="Enter \"Password\":")
+    passBox = Entry(window, width=20, validate="key")
+    passBox.configure(validatecommand=(passBox.register(testVal), '%P'))
+    knownLbl = Label(window, text="")
+    enterBtn = Button(window, text="Check Strength", command=checkStrength)
+    timeLbl = Label(window, text="")
+    fileMenu = Menu(window)
+    fileItems = Menu(fileMenu)
 
-passLbl = Label(window, text="Enter \"Password\":")
-passBox = Entry(window, width=20, validate="key")
-passBox.configure(validatecommand=(passBox.register(testVal), '%P'))
-knownLbl = Label(window, text="")
-enterBtn = Button(window, text="Check Strength", command=checkStrength)
-timeLbl = Label(window, text="")
-fileMenu = Menu(window)
-fileItems = Menu(fileMenu)
+    fileItems.add_command(label='Exit', command=window.destroy)
+    fileItems.add_command(label='About', command=about)
 
-fileItems.add_command(label='Exit', command=window.destroy)
-fileItems.add_command(label='About', command=about)
+    fileMenu.add_cascade(label='File', menu=fileItems)
+    passLbl.grid(column=0, row=0)
+    passBox.grid(column=0, row=1)
+    enterBtn.grid(column=1, row=1)
+    knownLbl.grid(column=0, row=2)
+    timeLbl.grid(column=0, row=3)
 
-fileMenu.add_cascade(label='File', menu=fileItems)
-passLbl.grid(column=0, row=0)
-passBox.grid(column=0, row=1)
-enterBtn.grid(column=1, row=1)
-knownLbl.grid(column=0, row=2)
-timeLbl.grid(column=0, row=3)
+    window.config(menu=fileMenu)
+    passBox.focus()
+    diff.set(1)
+    window.update()
 
-window.config(menu=fileMenu)
-passBox.focus()
-diff.set(1)
-window.update()
-
-window.mainloop()
+    window.mainloop()
