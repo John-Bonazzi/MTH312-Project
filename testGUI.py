fd0fa98bac6b5271960e54c1c72961266bad5316
Ben Brown <brownb4@mail.gvsu.edu>
2020-04-09 04:42:52 -0400

    Added text wrapping, disabling of entry box and button, and made window larger to accomidate for new text

--- testGUI.py
+++ testGUI.py
@@ -7,9 +7,8 @@ import re
 if __name__ == "__main__":
     window = Tk()
 
-
     window.title("MTH312 Project")
-    window.geometry('350x250')
+    window.geometry('575x300')
 
     diff = IntVar()
     # window.passDispFlag = True
@@ -25,26 +24,20 @@ def testVal(P):
 
 
 def passwordUpdate(message):
-    # if message == "GAME OVER":
-    #    enterBtn.configure(state="enabled")
-    #    passBox.configure(state="enabled")
-    #    window.passDispFlag = False
-    #    messagebox.showinfo("YOU WIN!", "The test took too long and timed out,\n "
-    #                                    "congratulations! Your Password is secure")
-    #    knownLbl.configure(text="")
-    # elif window.passDispFlag:
     knownLbl.configure(text=message)
     window.update()
 
 
 def checkStrength():
+    enterBtn.configure(state="disabled")
+    passBox.configure(state="disabled")
+    window.update()
     if len(passBox.get()) > 0:
         Password(passBox.get(), passwordUpdate)
-        # enterBtn.configure(state="disabled")
-        # passBox.configure(state="disabled")
-        # window.passDispFlag = True
     else:
         knownLbl.configure(text="enter a password to test")
+    enterBtn.configure(state="enabled")
+    passBox.configure(state="enabled")
 
 
 def about():
@@ -54,9 +47,8 @@ if __name__ == "__main__":
     passLbl = Label(window, text="Enter \"Password\":")
     passBox = Entry(window, width=20, validate="key")
     passBox.configure(validatecommand=(passBox.register(testVal), '%P'))
-    knownLbl = Label(window, text="")
+    knownLbl = Label(window, wraplength=550, text="")
     enterBtn = Button(window, text="Check Strength", command=checkStrength)
-    timeLbl = Label(window, text="")
     fileMenu = Menu(window)
     fileItems = Menu(fileMenu)
 
@@ -64,11 +56,16 @@ if __name__ == "__main__":
     fileItems.add_command(label='About', command=about)
 
     fileMenu.add_cascade(label='File', menu=fileItems)
-    passLbl.grid(column=0, row=0)
-    passBox.grid(column=0, row=1)
-    enterBtn.grid(column=1, row=1)
-    knownLbl.grid(column=0, row=2)
-    timeLbl.grid(column=0, row=3)
+#    passLbl.grid(column=0, row=0)
+#    passBox.grid(column=0, row=1)
+#    enterBtn.grid(column=0, row=2)
+#    knownLbl.grid(column=0, row=3)
+
+    passLbl.pack()
+    passBox.pack()
+    enterBtn.pack()
+    knownLbl.pack(fill=BOTH)
+
 
     window.config(menu=fileMenu)
     passBox.focus()
